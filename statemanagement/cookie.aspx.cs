c6222a4d46ae5325fe77b523ae23ef9b356ce6e0
anantha77 <89677393+anantha77@users.noreply.github.com>
2021-08-31 15:53:31 +0530

    Add files via upload

---
+++ statemanagement/cookie.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class cookie : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Cookies["a"] != null)
+        {
+            Response.Write("ur last page at" + Request.Cookies["a"].Value);
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        HttpCookie ck = new HttpCookie("a");
+        ck.Value = DateTime.Now.ToString();
+        Response.Cookies.Add(ck);
+    }
+}
---
+++ statemanagement/state.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class state : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnalter_Click(object sender, EventArgs e)
+    {
+       ViewState["Calendar1"] = System.Drawing.Color.BlueViolet;
+        string Calendar1 = (string)ViewState["Calendar1"];
+
+    }
+}
