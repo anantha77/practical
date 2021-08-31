f75574bd63a2a44160f9c5971507202fa6f23076
anantha77 <89677393+anantha77@users.noreply.github.com>
2021-08-31 15:46:05 +0530

    Add files via upload

---
+++ list/act.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="act.aspx.cs" Inherits="act" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ListBox ID="ListBox1" runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" SelectionMode="Multiple" Width="87px">
+            <asp:ListItem>1</asp:ListItem>
+            <asp:ListItem>2</asp:ListItem>
+            <asp:ListItem>3</asp:ListItem>
+            <asp:ListItem>4</asp:ListItem>
+            <asp:ListItem Value="5"></asp:ListItem>
+        </asp:ListBox>
+        <asp:ListBox ID="ListBox2" runat="server" SelectionMode="Multiple" Width="107px"></asp:ListBox>
+    
+    </div>
+        <asp:Button ID="btn1" runat="server" OnClick="btn1_Click" Text="&gt;&gt;" />
+        <asp:Button ID="btn2" runat="server" OnClick="btn2_Click" Text="&lt;&lt;" />
+    </form>
+</body>
+</html>
---
+++ list/act.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+public partial class act : System.Web.UI.Page
+{
+    public string s1;
+    int sindex;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+     
+    }
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        s1 = ListBox1.SelectedItem.ToString();
+        sindex = ListBox1.SelectedIndex;
+        ListBox2.Items.Add(s1);
+        
+        if (ListBox1!=null)
+        {
+            ListBox1.Items.RemoveAt(sindex);
+        }
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        s1 = ListBox2.SelectedItem.ToString();
+
+        sindex = ListBox2.SelectedIndex;
+        ListBox1.Items.Add(s1);
+        if (ListBox2 != null)
+        {
+            ListBox2.Items.RemoveAt(sindex);
+        }
+        //string s = "";
+        //for (int i = 0; i < ListBox2.Items.Count; i++)
+        //{
+        //    if (ListBox2.Items[i].Selected)
+        //    {
+        //        s = s +ListBox1.Items[i].Text;
+        //    } 
+        //}
+        //ListBox1.Text = s;
+    }
+}
