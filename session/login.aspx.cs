ac6a0441e5300befa5a919978e119f68d9cb5e4f
anantha77 <89677393+anantha77@users.noreply.github.com>
2021-08-31 15:56:36 +0530

    Add files via upload

---
+++ session/login.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        if(Login1.UserName=="admin" && Login1.Password=="admin")
+        {
+            Session["uname"] = Login1.UserName;
+            Response.Redirect("home.aspx");
+        }
+    }
+}
